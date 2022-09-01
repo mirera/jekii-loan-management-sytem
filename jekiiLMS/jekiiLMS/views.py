1f8e998780d73de6ce94743ebe0c2f70ac89f71d
enigma <enigma@Wilsons-MacBook-Pro.local>
2022-09-01 18:07:49 +0300

    implemented the user login logic#2

--- jekiiLMS/jekiiLMS/views.py
+++ jekiiLMS/jekiiLMS/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-
+# @login_required(login_url='/user/auth-login.html')
 def homepage(request):
     return render(request, 'index.html')
