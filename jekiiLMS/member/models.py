9b1df9ccc97ff84b3dcf0fcda6637d10cc8cd9ed
enigma <enigma@Wilsons-MacBook-Pro.local>
2022-09-06 19:06:47 +0300

    added the member model and registed  it#3

--- jekiiLMS/member/models.py
+++ jekiiLMS/member/models.py
@@ -1,3 +1,17 @@
+from tkinter import CASCADE
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+#the member model-- this are the borrowers
+class Member(models.Model):
+    first_name = models.CharField(max_length=500)
+    last_name = models.CharField(max_length=500)
+    id_no = models.CharField(max_length=10, unique=True)
+    phone_no = models.CharField(max_length=10, unique=True)
+    address = models.CharField(max_length=500)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    business_type = models.TextField()
+
+    def __str__(self):
+        return self.first_name
