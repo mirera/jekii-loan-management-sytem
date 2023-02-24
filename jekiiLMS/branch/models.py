068478bdccaed3013b1686f44717456fe207c28d
enigma <enigma@Enigmas-MacBook-Pro.local>
2023-02-24 12:36:38 +0300

    successfully rendered branches_list.html

--- jekiiLMS/branch/models.py
+++ jekiiLMS/branch/models.py
@@ -6,14 +6,16 @@ from decimal import Decimal
 
 #Branch model for a branch
 ACTIVE_CHOICES = (
-    ('yes','YES'),
-    ('no','NO'),
+    ('active','ACTIVE'),
+    ('inactive','INACTIVE'),
 )
 class Branch(models.Model):
     name = models.CharField(max_length=500)
+    phone = models.CharField(max_length=10, default='0700000000')
+    email= models.EmailField(default='email@mail.com')
     open_date = models.DateTimeField(auto_now_add=True)
     capital = models.DecimalField(decimal_places=2, max_digits=12, validators=[MinValueValidator(Decimal('0.01'))])
-    active = models.CharField(max_length=10, choices=ACTIVE_CHOICES)
+    status = models.CharField(max_length=10, choices=ACTIVE_CHOICES, default='inactive')
     notes = models.TextField(null=True, blank=True)
 
 
