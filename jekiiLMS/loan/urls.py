64ea94a3e416406d7370dd5e5a0e33b41ee4db6e
enigma <enigma@Enigmas-MacBook-Pro.local>
2023-02-28 17:53:15 +0300

    designed the loan product creation form

--- jekiiLMS/loan/urls.py
+++ jekiiLMS/loan/urls.py
@@ -1,8 +1,11 @@
-
-from django.contrib import admin
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    # path('', admin.site.urls),
-
+    # path('list', views.listLoanProduct, name='list'),
+    path('create', views.createLoanProduct, name='create'),
+    # path('edit/<str:pk>', views.editLoanProduct, name='edit'),
+    # path('view/<str:pk>', views.viewLoanProduct, name='view'),
+    # path('delete/<str:pk>', views.deleteLoanProduct, name='delete'),
 ]
--- jekiiLMS/loan/views.py
+++ jekiiLMS/loan/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import LoanProduct
+from .forms import LoanProductForm
 
-# Create your views here.
+
+
+
+#create Loan Product view starts
+def createLoanProduct(request):
+    form = LoanProductForm()
+    #processing the data
+    if request.method == 'POST':
+        LoanProduct.objects.create(
+            loan_product_name = request.POST.get('loan_product_name'),
+            minimum_amount= request.POST.get('minimum_amount'),
+            maximum_amount= request.POST.get('maximum_amount'),
+            loan_product_term = request.POST.get('loan_product_term'),
+            loan_term_period = request.POST.get('loan_term_period'),
+            repayment_frequency = request.POST.get('repayment_frequency'),
+            interest_type = request.POST.get('interest_type'),
+            interest_rate = request.POST.get('interest_rate'),
+            service_fee_type = request.POST.get('service_fee_type'),
+            service_fee_value = request.POST.get('service_fee_value'),
+            penalty_type = request.POST.get('penalty_type'),
+            penalty_value = request.POST.get('penalty_value'),
+            penalty_frequency = request.POST.get('penalty_frequency'),
+            status = request.POST.get('status'),
+            loan_product_description = request.POST.get('loan_product_description'),
+        )
+        #redirecting user to branches page(url name) after submitting form
+        return redirect('list')
+    context= {'form':form}
+    return render(request,'loan/loan-product-create.html', context)
+#create branch view ends
