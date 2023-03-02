a35c35701a50d041b66bb38e629ab2015df6d41a
enigma <enigma@Enigmas-MacBook-Pro.local>
2023-03-02 17:09:56 +0300

    rendered member detailed view

--- jekiiLMS/member/urls.py
+++ jekiiLMS/member/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    #path('members', views.listMembers, name='members'),
+    path('members', views.listMembers, name='members'),
     path('create-member', views.createMember, name='create-member'),
     #path('edit-member/<str:pk>', views.editMember, name='edit-member'),
-    #path('view-member/<str:pk>', views.viewMember, name='view-member'),
+    path('view-member/<str:pk>', views.viewMember, name='view-member'),
     #path('delete-member/<str:pk>', views.deleteMember, name='delete-member'),
 ]
--- jekiiLMS/member/views.py
+++ jekiiLMS/member/views.py
@@ -27,3 +27,22 @@ def createMember(request):
     context= {'form':form}
     return render(request,'member/create-member.html', context)
 #create member view ends
+
+# list member view starts 
+def listMembers(request):
+    members = Member.objects.all().order_by('date_joined')
+
+    #later on add a loan context so as to utilize them on the member table.
+    context = {'members': members}
+    return render(request, 'member/members-list.html', context)
+
+# list member view ends
+
+# view member view starts 
+def viewMember(request, pk):
+    member = Member.objects.get(id=pk)
+
+    context = {'member': member}
+    return render(request, 'member/member-view.html', context)
+
+# view member view ends
