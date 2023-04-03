2c067356fdf254d0bf6ac5f03e07e503190352a7
enigma <enigma@Enigmas-MacBook-Pro.local>
2023-04-03 16:36:51 +0300

    setting up ground for super admin dashboard

--- jekiiLMS/company/models.py
+++ jekiiLMS/company/models.py
@@ -9,13 +9,13 @@ from django.contrib.auth.models import User
 
 class Company(models.Model):
     admin = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    phone_no = models.CharField(max_length=10, unique=True)
-    logo = models.ImageField(default='default.png', upload_to='profile_photos/')
-    favicon = models.ImageField(default='default.png', upload_to='profile_photos/')
+    name = models.CharField(max_length=100, default='Loginit Credits Ltd')
+    phone_no = models.CharField(max_length=10, unique=True, default='0712345678')
+    logo = models.ImageField(default='default.png', upload_to='companies_logo/')
+    favicon = models.ImageField(default='default.png', upload_to='companies_logo/')
     date_joined = models.DateTimeField(auto_now_add=True)
-    address = models.CharField(max_length=100)
-    site_title = models.CharField(max_length=100)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    site_title = models.CharField(max_length=100, default='Lending to the unlendable')
 
     def __str__(self):
         return self.name
--- jekiiLMS/user/models.py
+++ jekiiLMS/user/models.py
@@ -14,8 +14,7 @@ class SuperAdmin(models.Model):
     id_no = models.CharField(max_length=10, unique=True)
     phone_no = models.CharField(max_length=10, unique=True)
     profile_photo = models.ImageField(default='default.png', upload_to='profile_photos/')
-    date_joined = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
+ 
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
